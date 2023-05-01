958226b346405b09070f94abff139d9c0594f44f
Tofu <kmyeongb@gmail.com>
2023-05-01 20:03:02 +0900

    feat: 단계별로 풀기: 입출력과 사칙연산

---
+++ 단계별로풀어보기/1_입출력과사칙연산/BOJ_10171.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_10171 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+
+    }
+}
---
+++ 단계별로풀어보기/1_입출력과사칙연산/BOJ_10172.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BOJ_10172 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
